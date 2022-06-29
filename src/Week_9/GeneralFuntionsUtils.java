6252cd5365e771049c1bfaaa585c9c1c62559c41
aymehmet <aymehmet1976@gmail.com>
2022-06-29 13:28:54 +0300

    week_11

--- src/Week_9/GeneralFuntionsUtils.java
+++ src/Week_9/GeneralFuntionsUtils.java
@@ -3,7 +3,8 @@ package Week_9;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Functions {
+public class GeneralFuntionsUtils {
+
 
     public static Client login(ArrayList<Client> clientDataBase) {
 
@@ -15,10 +16,12 @@ public class Functions {
             int inputClient_Id = userInput();
             System.out.println("please enter your password");
             int inputPassword = userInput();
+            int a = clientDataBase.size();
 
             for (int i = 0; i <= clientDataBase.size() - 1; i++) {
                 if (clientDataBase.get(i).client_Id == inputClient_Id && clientDataBase.get(i).password == inputPassword) {
                     System.out.println("Login Succesful");
+                    //currentClientindex = i;
                     loggedInClient = clientDataBase.get(i);
                     isClientFound = true;
                     break;
@@ -29,18 +32,36 @@ public class Functions {
             } else break;
         }
         return loggedInClient;
-
     }
 
+    public static Client findClient(int client_id, ArrayList<Client> clientDataBase){
+        for (Client eachClient:clientDataBase){
+            if (eachClient.client_Id==client_id){
+                return eachClient;
+            }
+        }
+        return null;
+    }
+
+    public static Client findClient( ArrayList<Client> clientDataBase){
+
+        System.out.println("Enter Client-ID to be transfered ");
+        int clientId_toTransfer = userInput();
+        Client tobetransferedClient=null;
+
+        for (Client eachClient:clientDataBase){
+            if (eachClient.client_Id==clientId_toTransfer){
+                tobetransferedClient=eachClient;
+                break;
+            }
+        }
+        return tobetransferedClient;
+    }
 
     public static int userInput() {
-
         int userInput = new Scanner(System.in).nextInt();
         return userInput;
-
     }
-
-
 
 
     public static int  menu (){
@@ -54,5 +75,4 @@ public class Functions {
         return userInput();
 
     }
-
 }
