1039b06fb9c893256ffb3553330791f64781dcc5
lgaid <lgaid2011@gmail.com>
2022-07-02 16:57:04 -0400

    Palindromes String task update

--- src/Week_10/StringPalindromes/Palindrome2_HLN.java
+++ src/Week_10/StringPalindromes/Palindrome2_HLN.java
@@ -3,7 +3,7 @@ package Week_10.StringPalindromes;
 import java.util.ArrayList;
 
 public class Palindrome2_HLN {
-
+/*
     public static ArrayList<String> getPalindromeList (String[] array){
         ArrayList<String> results = new ArrayList<>();
         String reversed = "";
@@ -20,6 +20,8 @@ public class Palindrome2_HLN {
     }
 
 
+
+ */
 
 
 }
