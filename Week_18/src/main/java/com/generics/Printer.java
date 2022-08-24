181dd3f6b47bf4d99b524e8970814887febfee32
aymehmet <aymehmet1976@gmail.com>
2022-08-24 16:44:42 +0300

    package created

---
+++ Week_18/src/main/java/com/generics/Printer.java
@@ -0,0 +1,14 @@
+package com.generics;
+
+public class Printer {
+
+    Integer thingtoPrint;
+
+    public Printer(Integer thingtoPrint) {
+        this.thingtoPrint = thingtoPrint;
+    }
+
+    public  void print(){
+        System.out.println(thingtoPrint);
+    }
+}
