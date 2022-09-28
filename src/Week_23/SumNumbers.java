0a66b468f4a63df0b901627db35a8c6430f4ed6c
aymehmet <aymehmet1976@gmail.com>
2022-09-29 00:45:26 +0300

    week23 is  modified

--- src/Week_23/SumNumbers.java
+++ src/Week_23/SumNumbers.java
@@ -7,19 +7,16 @@ import java.util.stream.LongStream;
 public class SumNumbers {
 
     public static void main(String[] args) {
-        System.out.println(sumofNumbers1(2,4));
-        System.out.println(sumofNumbers1(2,4));
-        System.out.println(sumofNumbers2(2,4));
+        System.out.println(sumofNumbers(1,3));
+        System.out.println(sumofNumbers1(1,3));
+        System.out.println(sumofNumbers2(1,3));
     }
 
     private static int sumofNumbers(int num1, int num2) {
         int result = 0;
-        if (num1<num2) {
-            result = num1 + num2 + sumofNumbers(num1 + 1, num2 - 1);
-        }
+        if (num1<num2)  result = num1 + num2 + sumofNumbers(num1 + 1, num2 - 1);
+        else if (num1==num2) result=result+num1;
         return result;
-
-
     }
 
     private static int sumofNumbers1(int num1, int num2) {
